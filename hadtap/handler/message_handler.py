eff92d7a3949edbe1fc90f4f9482e87c49764968
riotr <reveland92@gmail.com>
2019-06-03 19:57:05 +0200

    handle bad names

--- hadtap/handler/message_handler.py
+++ hadtap/handler/message_handler.py
@@ -1,6 +1,7 @@
 import logging
 from hadtap.provider.sheet_provider import SheetProvider
 from hadtap.handler.handler import Handler
+from gspread.exceptions import CellNotFound
 # from hadtap.handler.newcomer_handler import NewcomerHandler
 
 logger = logging.getLogger(__name__)
@@ -18,11 +19,14 @@ class MessageHandler(Handler):
             # return self.newcomer_handler.handle(user_id, message)
             pass
         else:
-            value = self.sheet_provider.get_value_for_item(message)
-            if value is None:
-                return 'Nincs ilyen arucikk.'
-            user_name = self.sheet_provider.get_name(user_id)
-            logger.debug('record value for user: %s, %s', value, user_name)
-            self.sheet_provider.record_fogyasztas(user_name, value)
-            logger.info('fogyasztas recorded for %s: %s', user_id, value)
-            return 'done.'
+            try:
+                value = self.sheet_provider.get_value_for_item(message)
+                if value is None:
+                    return 'Nincs ilyen arucikk.'
+                user_name = self.sheet_provider.get_name(user_id)
+                logger.debug('record value for user: %s, %s', value, user_name)
+                self.sheet_provider.record_fogyasztas(user_name, value)
+                logger.info('fogyasztas recorded for %s: %s', user_id, value)
+                return 'done.'
+            except CellNotFound:
+                return 'Valoszinuleg rossz nevet adtal meg, szolt Revinek.'
